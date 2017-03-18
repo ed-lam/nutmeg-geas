3bb70f61b924cd711c574f1a32dd9e729a74bc65
Graeme Gange <gkgange@unimelb.edu.au>
2017-03-18 23:07:23 +1100

    Started updating definition tracking to deal with intvars.

--- fzn/simplify.mli
+++ fzn/simplify.mli
@@ -1,19 +1,48 @@
 (** Problem simplification *)
+type val_id = int
+
+type kind = 
+  | Bool
+  | Int of Dom.t
 type irel = Ile | Ieq | Igt | Ine
+
+type def = 
+  | Def_none
+  | Def_blit of bool
+  | Def_ilit of int
+  (* Aliasing *)
+  | Def_eq of val_id
+  (* Boolean functions *)
+  | Def_bneg of val_id
+  | Def_at of val_id * irel * int
+  | Def_or of val_id array
+  | Def_and of val_id array
+  (* Arithmetic functions *)
+  | Def_sum of val_id array
+  | Def_prod of val_id array
+  | Def_max of val_id array
+  | Def_min of val_id array
+  | Def_b2i of val_id
+(*
 type bdef =
-  | Beq of Problem.bval_id
-  | Bneg of Problem.bval_id
+  | Bv_const of bool
+  | Bv_eq of Problem.bval_id
+  | Bv_neg of Problem.bval_id
   | At of Problem.ival_id * irel * int
 
-(*
-type ival_info = { id : ident ; dom : Dom.t ; ann : ann_expr list }
-type model = {
-  symbols : (ident, fzn_expr * ann_expr list) Hashtbl.t ;
-  ivals : ival_info DynArray.t ;
-  bvals : (ident * ann_expr list) DynArray.t ;
-  constraints : (cstr * ann_expr list) DynArray.t ;
-  mutable objective : (goal * ann_expr list)
-}
-*)
+type idef = 
+  | Iv_const of int
+  | Iv_eq of Problem.ival_id
+  | Iv_neg of Problem.ival_id
+  | Bool2int of Problem.bval_id
+  | Max of Problem.ival_id array
+  | Min of Problem.ival_id array
+  | Linex of (int * Problem.ival_id) array
 
-val simplify : Problem.t -> (bdef option array * Problem.t)
+type def =
+  | Bvar of bdef option
+  | Ivar of idef option
+*)
+type t = ((kind * def) array * Problem.t)
+
+val simplify : Problem.t -> t
